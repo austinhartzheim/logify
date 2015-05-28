5bdefd55d76304ed557c184a9b0b38b28ff81e7c
Austin Hartzheim <austin@austinhartzheim.me>
2015-05-28 17:55:35 +0000

    Test that the webhooks urls.py file also lets invalid URLs 404.

--- webhooks/tests/test_urls.py
+++ webhooks/tests/test_urls.py
@@ -12,3 +12,11 @@ class TestUrls(django.test.TestCase):
         self.assertEqual(response.status_code, 405)
         response = client.post('/webhooks/shopify/123/customer_create')
         self.assertEqual(response.status_code, 400)
+
+    def test_404(self):
+        '''
+        Test that invalid URLs return a 404 response.
+        '''
+        client = django.test.Client()
+        response = client.get('/webhooks/this_should_404')
+        self.assertEqual(response.status_code, 404)
